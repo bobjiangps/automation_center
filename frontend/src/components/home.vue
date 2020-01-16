580f9bdb6d102bc45c1cd1309dd8e0d30b0f1d5a
bobjiangps <jbsv43@sina.com>
2020-01-16 15:03:46 +0800

    get token from frontend

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -2,6 +2,16 @@
   <div>
     <h3>{{ msg }}</h3>
     <button type="button" @click='getProjects' :style="{ margin: '10px', padding: '5px' }">get projects</button>
+    <button type="button" @click='getToken' :style="{ margin: '10px', padding: '5px' }">sign in</button>
+    <p v-if="auth.length > 0">user logged in</p>
+    <table v-if="auth.length > 0" :style="{ border: '2px solid gray', borderRadius: '5px', padding: '10px' }" align='center'>
+      <tr v-for='a in auth'>
+        <td>{{ a.id }}</td>
+        <td>{{ a.username }}</td>
+        <td>{{ a.email }}</td>
+        <td>{{ a.token }}</td>
+      </tr>
+    </table>
     <table :style="{ border: '2px solid gray', borderRadius: '5px', padding: '10px' }" align='center'>
       <tr v-for='p in projects'>
         <td>{{ p.id }}</td>
@@ -20,13 +30,25 @@ export default {
   data () {
     return {
       msg: 'Projects in Automation Center',
-      projects: []
+      projects: [],
+      auth: []
     }
   },
 
   methods: {
     getProjects() {
       axios.get('http://127.0.0.1:8000/automation/api/projects/').then(response => {this.projects = response.data["results"], this.msg = 'get projects data from django api'});
+    },
+
+    getToken() {
+      axios.post('http://127.0.0.1:8000/automation/api/api-token-auth/', {
+          "username": "bo",
+          "password": "111111"
+      }).then(response => {
+          this.auth.push(response.data)
+      }).catch(error => {
+          console.log("Error login")
+          console.log(error)});
     }
   }
 }
