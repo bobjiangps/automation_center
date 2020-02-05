62b05ece0b875c5ea176d1f6e0022151d58c2b01
bobjiangps <jbsv43@sina.com>
2020-02-05 18:25:42 +0800

    add project owner table

--- modules/projects/models.py
+++ modules/projects/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Project(models.Model):
+
+    PROJECT_TYPES = (
+        ("internal", "internal"),
+        ("public", "public")
+    )
     name = models.CharField(max_length=100)
+    project_type = models.CharField(choices=PROJECT_TYPES, default=PROJECT_TYPES[0][0], max_length=100)
     create_time = models.DateTimeField(default=timezone.now)
     update_time = models.DateTimeField(default=timezone.now)
+    owner = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
