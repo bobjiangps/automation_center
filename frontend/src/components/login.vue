b700706da87319fc950266a5ac86e77080be3eec
bobjiangps <jbsv43@sina.com>
2020-04-19 17:37:57 +0800

    update login page, update v-model to v-decorator

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,37 +1,84 @@
 <template>
- <div>
-   <form class="login" @submit.prevent="login">
-     <h1>Sign in</h1>
-     <label>username</label>
-     <input required v-model="username" type="text" placeholder="Name"/>
-     <label>Password</label>
-     <input required v-model="password" type="password" placeholder="Password"/>
-     <button type="submit">Login</button>
-   </form>
-   <p style="color: red;">{{ msg }}</p>
- </div>
+  <div style="margin: 5% 30% 15% 30%;">
+    <h1>User Login</h1>
+    <a-form
+      id="components-form-normal-login"
+      :form="form"
+      class="login-form"
+      @submit.prevent="login"
+    >
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'username',
+            { rules: [{ required: true, message: 'Please input your username!' }] },
+          ]"
+          placeholder="Username"
+        >
+          <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'password',
+            { rules: [{ required: true, message: 'Please input your Password!' }] },
+          ]"
+          type="password"
+          placeholder="Password"
+        >
+          <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-checkbox
+          v-decorator="[
+            'remember',
+            {
+              valuePropName: 'checked',
+              initialValue: true,
+            },
+          ]"
+          style = "float: left;"
+        >
+          Remember me
+        </a-checkbox>
+        <a-button type="primary" html-type="submit" class="login-form-button" style="width: 50%; float: right;">
+          Log in
+        </a-button>
+      </a-form-item>
+    </a-form>
+    <p style="color: red;">{{ msg }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      username : '',
-      password : '',
       msg: ''
     };
   },
 
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal_login' });
+  },
+
   methods: {
-    login: function () {
-      const { username, password } = this
-      this.$store.dispatch('login', { username, password })
-        .then(() => this.$router.push('/'))
-        .catch(err => {
-          console.log(err)
-          this.msg = 'wrong username or password'
-        })
-    }
+    login(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          const { username, password } = values
+          this.$store.dispatch('login', { username, password })
+          .then(() => this.$router.push('/'))
+          .catch(err => {
+            console.log(err)
+            this.msg = 'wrong username or password'
+          })
+        }
+      });
+    },
   }
 };
 </script>
