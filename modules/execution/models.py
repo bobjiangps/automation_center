c706d542c1d719725a5d728a2bb65e5f1bcdb904
bobjiangps <jbsv43@sina.com>
2020-04-30 10:32:01 +0800

    add new model about os and env

--- modules/execution/models.py
+++ modules/execution/models.py
@@ -25,3 +25,33 @@ class Driver(models.Model):
     create_time = models.DateTimeField(default=timezone.now)
     update_time = models.DateTimeField(default=timezone.now)
     comment = models.CharField(max_length=2000)
+
+
+class MobileOS(models.Model):
+
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=100)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = "execution_mobile_os"
+
+
+class PlatformOS(models.Model):
+
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=100)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = "execution_platform_os"
+
+
+class TestEnvironment(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "execution_environment"
