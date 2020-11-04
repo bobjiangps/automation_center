8ae063a9675d4a6b45d2572e1589b322cc1652d3
bobjiangps <jbsv43@sina.com>
2020-11-04 18:18:44 +0800

    update test scripts vue file

--- frontend/src/components/projects/test-script.vue
+++ frontend/src/components/projects/test-script.vue
@@ -1,13 +1,12 @@
 <template>
   <div :style="{backgroundColor: '#FFF', margin: '20px'}">
-    <h2>test</h2>
-    <a-table :columns="columns" :data-source="data" class="components-table-demo-nested">
+    <a-table :columns="tableColumns" :data-source="tableData" class="components-table-demo-nested">
       <a slot="operation" slot-scope="text">Publish</a>
       <a-table
         slot="expandedRowRender"
         slot-scope="text"
-        :columns="innerColumns"
-        :data-source="innerData"
+        :columns="tableInnerColumns"
+        :data-source="tableInnerData"
         :pagination="false"
       >
         <span slot="status" slot-scope="text"> <a-badge status="success" />Finished </span>
@@ -33,60 +32,64 @@
 </template>
 
 <script>
-const columns = [
-  { title: 'Name', dataIndex: 'name', key: 'name' },
-  { title: 'Platform', dataIndex: 'platform', key: 'platform' },
-  { title: 'Version', dataIndex: 'version', key: 'version' },
-  { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
-  { title: 'Creator', dataIndex: 'creator', key: 'creator' },
-  { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
-  { title: 'Action', key: 'operation', scopedSlots: { customRender: 'operation' } },
-];
-
-const data = [];
-for (let i = 0; i < 3; ++i) {
-  data.push({
-    key: i,
-    name: 'Screem',
-    platform: 'iOS',
-    version: '10.3.4.5654',
-    upgradeNum: 500,
-    creator: 'Jack',
-    createdAt: '2014-12-24 23:12:00',
-  });
-}
-
-const innerColumns = [
-  { title: 'Date', dataIndex: 'date', key: 'date' },
-  { title: 'Name', dataIndex: 'name', key: 'name' },
-  { title: 'Status', key: 'state', scopedSlots: { customRender: 'status' } },
-  { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
-  {
-    title: 'Action',
-    dataIndex: 'operation',
-    key: 'operation',
-    scopedSlots: { customRender: 'operation' },
-  },
-];
-
-const innerData = [];
-for (let i = 0; i < 3; ++i) {
-  innerData.push({
-    key: i,
-    date: '2014-12-24 23:12:00',
-    name: 'This is production name',
-    upgradeNum: 'Upgraded: 56',
-  });
-}
-
 export default {
+  name: 'ProjectTestScripts',
   data() {
     return {
-      data,
-      columns,
-      innerColumns,
-      innerData,
+      tableData: '',
+      tableColumns: '',
+      tableInnerData: '',
+      tableInnerColumns: ''
     };
   },
+
+  created: function() {
+    this.tableColumns = [
+      { title: 'Name', dataIndex: 'name', key: 'name' },
+      { title: 'Platform', dataIndex: 'platform', key: 'platform' },
+      { title: 'Version', dataIndex: 'version', key: 'version' },
+      { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+      { title: 'Creator', dataIndex: 'creator', key: 'creator' },
+      { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
+      { title: 'Action', key: 'operation', scopedSlots: { customRender: 'operation' } },
+    ];
+
+    this.tableData = [];
+    for (let i = 0; i < 3; ++i) {
+      this.tableData.push({
+        key: i,
+        name: 'Screem',
+        platform: 'iOS',
+        version: '10.3.4.5654',
+        upgradeNum: 5001,
+        creator: 'Jack',
+        createdAt: '2014-12-24 23:12:00',
+      });
+    }
+
+    this.tableInnerColumns = [
+      { title: 'Date', dataIndex: 'date', key: 'date' },
+      { title: 'Name', dataIndex: 'name', key: 'name' },
+      { title: 'Status', key: 'state', scopedSlots: { customRender: 'status' } },
+      { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+      {
+        title: 'Action',
+        dataIndex: 'operation',
+        key: 'operation',
+        scopedSlots: { customRender: 'operation' },
+      },
+    ];
+
+    this.tableInnerData = [];
+    for (let i = 0; i < 3; ++i) {
+      this.tableInnerData.push({
+        key: i,
+        date: '2014-12-24 23:12:00',
+        name: 'This is production name',
+        upgradeNum: 'Upgraded: 56',
+      });
+    }
+  },
+
 };
 </script>
