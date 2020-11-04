d3d7b7679fff8602a61259ee4fd02b79617e8e2e
bobjiangps <jbsv43@sina.com>
2020-11-04 18:17:46 +0800

    add url for test scripts of project

--- modules/projects/urls.py
+++ modules/projects/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path(r'mock_automation_coverage/', views.MockAutomationCoverageData.as_view(), name='mock_project_automation_coverage'),
     # path(r'in_url/project/<int:project_id>/user/<int:user_id>/', views.ExampleParameterInUrl.as_view(), name='example_parameter_in_url'),
     # path(r'in_request/', views.ExampleParameterInRequest.as_view(), name='example_parameter_in_request'),
+    path(r'<int:project_id>/test-scripts/', views.ScriptList.as_view(), name='script_list'),
+    path(r'<int:project_id>/test-scripts/<int:pk>/', views.ScriptDetail.as_view(), name='script_detail'),
 ]
