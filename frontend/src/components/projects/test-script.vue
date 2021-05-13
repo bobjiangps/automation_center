a477e1011b95dd0acdf9c696a4fc2b5c93161672
bobjiangps <jbsv43@sina.com>
2021-05-13 22:10:30 +0800

    update the test scripts vue to show scripts and cases

--- frontend/src/components/projects/test-script.vue
+++ frontend/src/components/projects/test-script.vue
@@ -4,9 +4,19 @@
       :columns="tableColumns"
       :data-source="tableData"
       :loading="loading"
-      class="components-table-demo-nested"
+      @expand="retrieveCases"
+      class="components-table-nested"
     >
-      <a slot="operation" slot-scope="text">Publish</a>
+      <span slot="customTitle"><a-icon type="check-circle" theme="twoTone" two-tone-color="#52c41a" />&ensp;&ensp;Name</span>
+      <span slot="tagsCustom" slot-scope="tags">
+        <a-tag
+          v-for="tag in tags"
+          :key="tag"
+          :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+        >
+          {{ tag.toUpperCase() }}
+        </a-tag>
+      </span>
       <a-table
         slot="expandedRowRender"
         slot-scope="text"
@@ -15,24 +25,8 @@
         :pagination="false"
       >
         <span slot="status" slot-scope="text"> <a-badge status="success" />Finished </span>
-        <span slot="operation" slot-scope="text" class="table-operation">
-          <a>Pause</a>
-          <a>Stop</a>
-          <a-dropdown>
-            <a-menu slot="overlay">
-              <a-menu-item>
-                Action 1
-              </a-menu-item>
-              <a-menu-item>
-                Action 2
-              </a-menu-item>
-            </a-menu>
-            <a> More <a-icon type="down" /> </a>
-          </a-dropdown>
-        </span>
       </a-table>
     </a-table>
-
   </div>
 </template>
 
@@ -43,9 +37,10 @@ export default {
     return {
       tableData: [],
       tableColumns: '',
-      tableInnerData: '',
+      tableInnerData: [],
       tableInnerColumns: '',
       scripts: '',
+      cases: '',
       loading: false
     };
   },
@@ -53,59 +48,78 @@ export default {
   created: function() {
 
     this.tableColumns = [
-      { title: 'Name', dataIndex: 'name', key: 'name', sorter: true, scopedSlots: { customRender: 'name' } },
+      { dataIndex: 'name', key: 'name', sorter: true, slots: { title: 'customTitle' } },
       { title: 'Version', dataIndex: 'version', key: 'version' },
-      { title: 'Status', dataIndex: 'status', key: 'status' },
+      { title: 'Status', dataIndex: 'status', key: 'status', sorter: true },
       { title: 'Author', dataIndex: 'author', key: 'author' },
       { title: 'Maintainer', dataIndex: 'maintainer', key: 'maintainer' },
-      { title: 'Tag', dataIndex: 'tag', key: 'tag' },
+      { title: 'Tags', dataIndex: 'tags', key: 'tags', scopedSlots: { customRender: 'tagsCustom' }, },
     ];
 
     this.tableInnerColumns = [
-      { title: 'Date', dataIndex: 'date', key: 'date' },
-      { title: 'Name', dataIndex: 'name', key: 'name' },
-      { title: 'Status', key: 'state', scopedSlots: { customRender: 'status' } },
-      { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
-      {
-        title: 'Action',
-        dataIndex: 'operation',
-        key: 'operation',
-        scopedSlots: { customRender: 'operation' },
-      },
+      { title: 'Case ID', dataIndex: 'caseid', key: 'caseid' },
+      { title: 'Case Name', dataIndex: 'casename', key: 'casename' },
+      { title: 'Create Time', dataIndex: 'createtime', key: 'createtime' },
+      { title: 'Update Time', dataIndex: 'updatetime', key: 'updatetime' },
     ];
 
-    this.tableInnerData = [];
-    for (let i = 0; i < 3; ++i) {
-      this.tableInnerData.push({
-        key: i,
-        date: '2014-12-24 23:12:00',
-        name: 'This is production name',
-        upgradeNum: 'Upgraded: 56',
-      });
-    }
   },
 
   mounted: function() {
-    this.loading = true;
-    this.$http.get(`${this.$http.defaults.baseURL}/projects/${this.$route.params.project_id}/test-scripts/`)
-      .then(response => {
-        this.scripts = response.data["results"];
-        for (let i = 0; i < this.scripts.length; i++) {
-          var script = this.scripts[i];
-          this.tableData.push({
-            key: i,
-            name: script["name"],
-            version: script["version"],
-            status: script["status"],
-            author: script["author"],
-            maintainer: script["maintainer"],
-            tag: script["tag"],
-          });
-        }
-        this.loading = false;
-      })
-      .catch(err => {console.log(err)})
+    this.retrieveScripts();
   },
+
+  methods: {
+    retrieveScripts() {
+      this.loading = true;
+      this.$http.get(`${this.$http.defaults.baseURL}/projects/${this.$route.params.project_id}/test-scripts/`)
+        .then(response => {
+          this.scripts = response.data["results"];
+          for (let i = 0; i < this.scripts.length; i++) {
+            var script = this.scripts[i];
+            this.tableData.push({
+              key: i,
+              id: script["id"],
+              name: script["name"],
+              version: script["version"],
+              status: script["status"],
+              author: script["author"],
+              maintainer: script["maintainer"],
+              tags: script["tag"].split(","),
+            });
+          }
+          this.loading = false;
+        })
+        .catch(err => {console.log(err)});
+    },
+
+    retrieveCases(expanded, record) {
+      if (expanded) {
+        this.$http.get(`${this.$http.defaults.baseURL}/projects/${this.$route.params.project_id}/test-scripts/${record.id}/automation-case`)
+          .then(response => {
+            if (this.cases.length > 0) {
+              this.cases.splice(0, this.cases.length);
+              this.tableInnerData.splice(0, this.tableInnerData.length);
+            }
+            this.cases = response.data["results"];
+            for (let i = 0; i < this.cases.length; i++) {
+              var autoCase = this.cases[i];
+              var autoCaseNameSplit = autoCase["name"].split(":");
+              this.tableInnerData.push({
+                key: i,
+                caseid: autoCaseNameSplit[0],
+                casename: autoCaseNameSplit[1].trim(),
+                createtime: autoCase["create_time"],
+                updatetime: autoCase["update_time"]
+              });
+            }
+            this.loading = false;
+          })
+          .catch(err => {console.log(err)});
+      }
+    }
+
+  }
 
 };
 </script>
