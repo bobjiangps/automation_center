c98592c8de5b4a1e908771fddce2a91328b4c4cc
bobjiangps <jbsv43@sina.com>
2021-06-01 18:06:45 +0800

    register execution models in admin

--- modules/execution/admin.py
+++ modules/execution/admin.py
@@ -1,3 +1,10 @@
+from .models import Browser, Device, Driver, MobileOS, PlatformOS, TestEnvironment
 from django.contrib import admin
 
-# Register your models here.
+
+admin.site.register(Browser)
+admin.site.register(Device)
+admin.site.register(Driver)
+admin.site.register(MobileOS)
+admin.site.register(PlatformOS)
+admin.site.register(TestEnvironment)
